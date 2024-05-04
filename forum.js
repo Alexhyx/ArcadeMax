a91661ca0a392a83de1e29e63a62f7e9f2d741cc
Catalina <arreolc2@uci.edu>
2024-05-03 20:25:27 -0700

    Finalized Forum
    Created the option to view a single post, and go back to all posts. Can now comment on the posts as well

--- forum.js
+++ forum.js
@@ -2,16 +2,22 @@
 
 let posts =
 [
-    {title: "Sample Post", message_author: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum, ligula ut fermentum aliquam, urna ipsum lacinia nulla, vel aliquam odio felis in leo. Duis in elit ac erat convallis efficitur.", "Anon"], likes: 0}
+    {title: "Sample Post", 
+    message_author: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam condimentum, ligula ut fermentum aliquam, urna ipsum lacinia nulla, vel aliquam odio felis in leo. Duis in elit ac erat convallis efficitur."], 
+    likes: 0, 
+    comments:["Cool", "Very Cool!"],
+    user: "Anon"}
 ]
 
 const postsOnPage = 3;
 let currentPage = 0;
+let currentState = "allPosts";
 
 
 function displayPosts() {
-    const postsContainer = document.getElementById("posts");
-    postsContainer.innerHTML = ""; 
+    const postsHTML = document.getElementById("posts");
+    postsHTML.innerHTML = "";
+    currentState = "allPosts" 
 
     const startIndex = (currentPage) * postsOnPage;
     const endIndex = startIndex + postsOnPage;
@@ -24,9 +30,11 @@ function displayPosts() {
         postElement.innerHTML =
             `<h3>${post.title}</h3>
             <p>${post.message_author[0]}</p>
-            <p style = "font-size: 50%;" >By: ${post.message_author[1]}</p>
-            <button class="like-count" onclick="likePost(${startIndex + posts.indexOf(post)})">Like ${post.likes}</button>`;
-        postsContainer.appendChild(postElement);
+            <p style = "font-size: 50%;" >By: ${post.user}</p>
+            <button class="like-count" onclick="likePost(${startIndex + posts.indexOf(post)})">Like ${post.likes}</button>
+            <button class="like-count" onclick="displaySinglePost(${startIndex + posts.indexOf(post)})">View Post</button>
+            <button class="like-count" onclick="displaySinglePost(${startIndex + posts.indexOf(post)})">Comment</button>`;
+        postsHTML.appendChild(postElement);
     } );
 
    
@@ -39,7 +47,7 @@ function createNewPost() {
     const user = "Anon";
 
     //console.log(postTitle, postMessage)
-    posts.unshift({title:postTitle, message_author:[postMessage,user], likes:0})
+    posts.unshift({title:postTitle, message_author:[postMessage,user], likes:0, comments:[], user:"Anon"})
     
     displayPosts();
     
@@ -52,8 +60,62 @@ function createNewPost() {
 
 function likePost(index) {
     posts[index].likes = posts[index].likes+1;
+    if (currentState == "allPosts"){
+        displayPosts();
+    }
+    else{
+        displaySinglePost(index);
+    }
+}
 
+function displaySinglePost(index){
+    const postsHTML = document.getElementById("posts");
+    postsHTML.innerHTML = "";
+    currentState = "singlePost";
+    let post = posts[index];
+
+
+    
+    const postElement = document.createElement("div");
+    postElement.innerHTML =`
+        <button class = "backButton" onclick = "back()">Go Back</button>
+        <div id = "singlePost">
+        <h3>${post.title}</h3>
+        <p>${post.message_author[0]}</p>
+        <p style = "font-size: 50%;" >By: ${post.message_author[1]}</p>
+        <button class="like-count" onclick="likePost(${index})">Like ${post.likes}</button>
+        </div>
+        <div id = "personalComment">
+        <textarea id="CommentTextArea" name="postMessage"></textarea><br><br>
+        <button class = "commentButton" onclick = "commentPost(${index})">Comment</button>
+        </div>`;
+    postsHTML.appendChild(postElement);
+
+    post.comments.forEach(comment => {
+        const postComment = document.createElement("div");
+        
+        postComment.innerHTML =`
+            <p style = "font-size: 50%;">${comment}</p>
+            `;
+        console.log(postComment)
+        postsHTML.appendChild(postComment);
+    } )
+    
+
+
+    
+}
+function back(){
+    currentState = "allPosts";
     displayPosts();
+}
+
+function commentPost(index){
+    let post = posts[index];
+    let newComment = document.getElementById("CommentTextArea").value;
+    post.comments.unshift(newComment);
+
+    displaySinglePost(index);
 }
 
 displayPosts();
