a203b9fab75a9cfc0b654156aaca6140cb60be70
Catalina <arreolc2@uci.edu>
2024-05-09 20:57:02 -0700

    Starting the project, we added a navbar

---
+++ arcademax/src/components/HEADERR/LoginButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginButton = () => {
+
+
+    // When you click by default it'll take you to login, with the option to sign up
+    // Idk how to link in react
+    return(
+        <div class="auth-buttons">
+            <button id="loginBtn">Login/Sign Up</button>
+        </div>
+    );
+}
