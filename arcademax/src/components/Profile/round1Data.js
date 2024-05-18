498ef74352cb39c7b81c49667c67533fc67792df
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2024-05-17 19:13:21 -0700

    Added Stub Data for Users and Arcade Owner - Can Edit ArcadeOwner Profile's Locations - Can't Add New Location Yet

---
+++ arcademax/src/components/Profile/round1Data.js
@@ -0,0 +1,16 @@
+const round1Data = [
+    {
+        id: 1,
+        name: "Round 1 @ Santa Ana",
+        address: "2800 N Main St, Santa Ana, CA, US, 92705",
+        games: ["Pump it Up", "Dance Dance Revolution", "Arcane Games"],
+    },
+    {
+        id: 2,
+        name: "Round 1 @ Mission Viejo",
+        address: "60C The Shops at Mission Viejo Mission Viejo, CA, US 92691",
+        games: ["Pump it Up", "Dance Dance Revolution", "Arcane Games"],
+    },
+    
+];
+export default round1Data;
