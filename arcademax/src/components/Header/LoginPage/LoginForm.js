22cd7d2e81894f4f54018a6ee0c8bfe3783357b6
Catalina <arreolc2@uci.edu>
2024-05-18 11:55:57 -0700

    Connected the login page to the profile page

--- arcademax/src/components/Header/LoginPage/LoginForm.js
+++ arcademax/src/components/Header/LoginPage/LoginForm.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoginForm.css'; 
+import Profile from '../../Profile/ProfilePage';
 
-function LoginForm({ onLogin }) {
+function LoginForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleUsernameChange = (event) => {
+    const navigate = useNavigate();
+
+    const UsernameChange = (event) => {
         setUsername(event.target.value);
     };
 
-    const handlePasswordChange = (event) => {
+    const PasswordChange = (event) => {
         setPassword(event.target.value);
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
       
-        if(onLogin) {
-            onLogin(username, password);
-        }
+        navigate('/ProfilePage', { state: { username: username } });
         console.log('Login Submitted:', username, password);
     };
 
@@ -27,11 +29,11 @@ function LoginForm({ onLogin }) {
             <form onSubmit={handleSubmit} className="login-form">
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange} required />
+                    <input type="text" id="username" name="username" value={username} onChange={UsernameChange} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required />
+                    <input type="password" id="password" name="password" value={password} onChange={PasswordChange} required />
                 </div>
                 <div className="form-actions">
                     <button type="submit">Login</button>
