ac233f12fb1f6859dcf3b1579ec0dda4a2c83e38
SPerezdeTudela <sperezde@uci.edu>
2024-05-17 20:10:57 -0700

    Added header

---
+++ arcademax/src/components/HEADERR/ProfileButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LoginButton = () => {
+
+
+    //Somehow when you've logged in, will change button display
+    return(
+        <div class="auth-buttons">
+            <button id="profileBtn">Profile</button>
+        </div>
+    );
+}
+
+
+export default LoginButton;
+
+
