44458351771699b997f201ed43b6c4685fe778ea
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2024-05-19 02:55:19 +0000

    Add style to Search Page

--- arcademax/src/components/Search/GameLocationDropdowns.js
+++ arcademax/src/components/Search/GameLocationDropdowns.js
@@ -1,49 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useArcadeInfoHandler } from "../../stubData/ArcadeInfoHandler";
-import { useState } from "react";
 import './GameLocationDropdowns.css';
 
-
-const GameLocationDropdowns = ({onLocationSelect}) =>{
-    const {gameLocations} = useArcadeInfoHandler();
-    const [games] = useState(Object.keys(gameLocations))
+const GameLocationDropdowns = ({ onLocationSelect }) => {
+    const { gameLocations } = useArcadeInfoHandler();
+    const [games] = useState(Object.keys(gameLocations));
     const [showLocations, setShowLocations] = useState(false);
-    const [locations, setLocations] = useState([])
+    const [locations, setLocations] = useState([]);
 
-   
-
-    const displayLocations =(e)=>{
+    const displayLocations = (e) => {
         console.log(e.target.value);
         const key = e.target.value;
         setShowLocations(true);
         setLocations(gameLocations[key]);
-    }
-    
-    const changeMap = (location) =>{
+    };
+
+    const changeMap = (location) => {
         console.log(location);
         onLocationSelect(location);
-    }
-    
+    };
 
-    return(
+    return (
         <div className="dropdowns">
-            <select id = "allGames" onChange={displayLocations}>
-                {games.map((game, index)=>(
-                    <option key = {index} value = {game}> {game} </option>
+            <select id="allGames" onChange={displayLocations} defaultValue="">
+                <option value="" disabled hidden>Select a game</option>
+                {games.map((game, index) => (
+                    <option key={index} value={game}> {game} </option>
                 ))}
             </select>
-            
+
             {showLocations && (
-                <ul>
-                {locations.map((location, index)=>(
-                    <>
-                    <li key={index} value={location} onClick={() => changeMap(location)} className="list-item">{location}</li>
-                    </>
-                    
-                ))}
+                <ul className="address-list">
+                    {locations.map((location, index) => (
+                        <li key={index} value={location} onClick={() => changeMap(location)} className="list-item">
+                            {location}
+                        </li>
+                    ))}
                 </ul>
             )}
-            
         </div>
     );
 };
--- arcademax/src/components/Search/search.js
+++ arcademax/src/components/Search/search.js
@@ -3,22 +3,33 @@ import { ArcadeInfoProvider } from '../../stubData/ArcadeInfoHandler';
 import GameLocationDropdowns from './GameLocationDropdowns';
 import Map from './Map';
 import ArcadeInputForm from '../ArcadeInput/ArcadeInput';
+import './search.css';
 
 function Search() {
     const [selectedLocation, setSelectedLocation] = useState('');
+    const [showPopup, setShowPopup] = useState(false); // Step 2
+
+    // Step 3: Function to toggle pop-up visibility
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    };
 
     return (
         <div>
+            <h1 className='title' id='search-title'>Arcade Search</h1>
             <ArcadeInfoProvider>
                 <Map location={selectedLocation} />
                 <GameLocationDropdowns onLocationSelect={setSelectedLocation} />
             </ArcadeInfoProvider>
             
-            <ArcadeInputForm/>
-            
+            {/* Pop-up Window for Arcade Input Form */}
+            <div className="centered-container"> {/* Container to center the button */}
+                <button onClick={togglePopup}>Show Input Form</button>
+            </div>
+            {showPopup && <ArcadeInputForm onClose={togglePopup} />}
+
         </div>
     );
 }
 
 export default Search;
-
