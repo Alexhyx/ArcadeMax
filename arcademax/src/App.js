b77a92da531f24c88bb6308eb17f303b8cc87609
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2024-06-10 01:09:10 -0700

    User can only add post if logged in

--- arcademax/src/App.js
+++ arcademax/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { auth } from './firebase';
 
 import Search from './components/Search/search';
 import NavBar from './components/Header/NavBar';
@@ -9,28 +10,33 @@ import Forum from './components/Forum/Forum';
 import AboutPage from './components/About/About';
 import LoginForm from './components/LoginPage/LoginForm';
 
-
 function App() {
   // const userId = 1 //regular ID
   const userId = 3; //arcade Owner ID
+
+  const [signedIn, setSignedIn] = useState(false);
+
+  useEffect(() => {
+    const userState = auth.onAuthStateChanged(user => {
+      setSignedIn(!!user);
+    });
+
+    return () => userState();
+  }, []);
+
   return (
     <BrowserRouter>
       <NavBar/>
       <Routes>
         <Route path="/" element={<Search/>} />
-        <Route path="/Forum" element={<Forum />} />
+        <Route path="/Forum" element={<Forum signedIn={signedIn} />} />
         <Route path="/ProfilePage" element={<Profile userId={userId} />}/>
         <Route path="/About" element={<AboutPage/>}/>
         <Route path="/LoginForm" element={<LoginForm/>}/>
 
       </Routes>
     </BrowserRouter>
-    
-
-      
-        
   );
-  
 }
 
 export default App;
